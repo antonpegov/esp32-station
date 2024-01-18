3f4e3e04bbbd66246ea69bcd6cdf5faaef30dac0
Anton Pegov <antonpegov@gmail.com>
2024-01-18 13:56:03 +0100

    Move configs to config.h

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,10 @@
 #include <Wire.h>
 
 #include "blinkers.h"
+#include "config.h"
 #include "display.h"
 #include "helpers.h"
 
-#define BUTTON1_Pin 35
-#define BUTTON2_Pin 36
-#define BUTTON3_Pin 37
-#define LED1_Pin 20
-#define LED1_High 1000
-#define LED1_Low 5000
-#define LED2_Pin 21
-#define LED2_High 1500
-#define LED2_Low 3000
-#define DISPLAY_Refresh 3000
-#define DISPLAY_Brightness 7
-
-#define SERIAL_Baud 9600
-#define TM1637_DIO_Pin 1
-#define TM1637_CLK_Pin 2
-#define SDA_Pin 15
-#define SCL_Pin 16
-#define SHT31_Address 0x44
 // Define enum for all display modes
 enum DisplayMode { Temperature, Humidity, Time };
 
